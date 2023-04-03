5d521a106e2367b1481b86909a3407bd42e87786
Enu Azubike <enuazubike88@gmail.com>
2023-04-03 22:41:54 +0100

    fixed CI builds

--- mssc-brewery-client/src/test/java/com/azubike/msscbreweryclient/web/client/BreweryClientTest.java
+++ mssc-brewery-client/src/test/java/com/azubike/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,6 @@
 package com.azubike.msscbreweryclient.web.client;
 
-import com.azubike.msscbreweryclient.utils.TestUtils;
+
 import com.azubike.msscbreweryclient.web.model.BeerDto;
 import com.azubike.msscbreweryclient.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,7 +18,16 @@ class BreweryClientTest {
 
   @Test
   void getById() {
-    final BeerDto beerDto = breweryClient.getById(UUID.randomUUID());
+    BeerDto request = BeerDto.builder()
+            .beerStyle(BeerStyle.ALE)
+            .beerName("Gulder")
+            .price(new BigDecimal("1.23"))
+            .upc("13241214")
+            .build();
+
+    BeerDto response = breweryClient.saveNewBeer(request);
+
+    final BeerDto beerDto = breweryClient.getById(response.getId());
     assertThat(beerDto).isNotNull();
   }
 
