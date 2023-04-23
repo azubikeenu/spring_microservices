5a72b2031fa7ad0f011c6240500d88a05eb886d4
Enu Azubike <enuazubike88@gmail.com>
2023-04-23 01:46:54 +0100

    added activeMQ config for services

---
+++ mssc-beer-service/src/main/java/com/azubike/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,19 @@
+package com.azubike.msscbeerservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MessageConverter messageConverter(){
+        // this creates a bidirectional mapping between our POJO objects to JSON
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+         converter.setTargetType(MessageType.TEXT);
+         converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
