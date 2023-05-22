953a60c5ef320d29826ecb60d6d3ff13be8e1f5d
Enu Azubike <enuazubike88@gmail.com>
2023-05-22 19:14:15 +0100

    added eureka server

--- mssc-brewery-gateway/src/main/java/com/azubike/ellipsis/config/GoogleConfig.java
+++ mssc-brewery-gateway/src/main/java/com/azubike/ellipsis/config/GoogleConfig.java
@@ -4,15 +4,12 @@ package com.azubike.ellipsis.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("google")
-@Configuration
+//@Profile("google")
+//@Configuration
 public class GoogleConfig {
     @Bean
     RouteLocator googleRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
-        System.out.println("############### I RAN###################");
         return routeLocatorBuilder
                 .routes().
                 route(r ->
